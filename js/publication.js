3f3d3f4808aead622a0db5d0d4b6d57b653c6e9b
whduddhks <jyy1551@naver.com>
2023-12-25 09:42:52 +0900

    publication changes from source

--- js/publication.js
+++ js/publication.js
@@ -1,6 +1,59 @@
-window.onload = pageLoad;
-function pageLoad() {
-    var margin_value = document.querySelector('.navbar').scrollHeight;
-    document.querySelector('.wrapper').style.marginTop = String(margin_value) + 'px';
-    document.querySelector('.position-sticky').style.top = String(margin_value) + 'px';
+let page = document.querySelector('#page-title').innerHTML;
+let paper = document.querySelector('.paper-div');
+
+let now;
+
+if (page == 'International Conferences') {
+    now = 'ipc';
+} else if (page == 'International Journals') {
+    now = 'ipj';
+} else if (page == 'International Patents') {
+    now = 'ip';
+} else if (page == 'Domestic Conferences') {
+    now = 'dpc';
+} else if (page == 'Domestic Journals') {
+    now = 'dpj';
+} else {
+    now = 'dp';
 };
+
+let testdata = JSON.parse(JSON.stringify(data));
+let temp_data = testdata[now];
+var keys = Object.keys(temp_data);
+keys.reverse();
+
+for (var i=0; i < keys.length; i++) {
+    let sec_tag = document.createElement('section');
+    sec_tag.className = 'year-section';
+    sec_tag.id = keys[i];
+
+    let h4_year = document.createElement('h4');
+    h4_year.className = "year";
+    h4_year.innerHTML = keys[i];
+
+    sec_tag.appendChild(h4_year);
+
+    for (var j=0; j < temp_data[keys[i]].length; j++) {
+        let a_tag = document.createElement('a');
+        a_tag.className = 'paper-link';
+        a_tag.href = temp_data[keys[i]][j]['link'];
+        a_tag.innerHTML = temp_data[keys[i]][j]['title'];
+
+        let h4_tag = document.createElement('h4');
+        h4_tag.className = 'title';
+        h4_tag.appendChild(a_tag);
+
+        let p_tag = document.createElement('p');
+        p_tag.className = 'info';
+        p_tag.innerHTML = temp_data[keys[i]][j]['author'] + "<br>" + keys[i] + ", " + temp_data[keys[i]][j]['submit'];
+
+        let div_tag = document.createElement('div');
+        div_tag.className = 'list-item';
+        div_tag.appendChild(h4_tag);
+        div_tag.appendChild(p_tag);
+    
+        sec_tag.appendChild(div_tag);
+    }
+
+    paper.appendChild(sec_tag);
+}
